f5fbaf17290f3d7d6c8736d2c1e46044f5395d8c
jpatel@pivotal.io <jpatel@pivotal.io>
2016-07-14 11:53:58 -0700

    refactor out and printing of request

--- google_drive_concourse_resource_common.py
+++ google_drive_concourse_resource_common.py
@@ -141,7 +141,7 @@ def getFile(service, folderID, fileID, verbose=False):
     request = drive_service.files().get_media(fileId=fileID)
 
     if verbose:
-        print(request)
+        print(request.text)
 
     media_request = http.MediaIoBaseDownload('test.txt', request)
 
