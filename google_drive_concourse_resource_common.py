bc7d202ce64f76e8f07a21ba43f34977356b998e
jpatel@pivotal.io <jpatel@pivotal.io>
2016-07-20 12:55:42 -0700

    using Drive API v2 to get modifiedDate

--- google_drive_concourse_resource_common.py
+++ google_drive_concourse_resource_common.py
@@ -72,7 +72,7 @@ def listFilesinFolder(service, folderID, fileName, verbose=False):
         latest = ''
         for item in items:
             print('Name: {0} ID: ({1}) MimeType: {2} ModTime: {3}'.format(item['title'], item['id'], item['mimeType'], item['modifiedTime']),file=sys.stderr)
-            mTime = item['modifiedTime']
+            mTime = item['modifiedDate']
             if latest is None:
                 latest = mTime
             elif latest < mTime:
